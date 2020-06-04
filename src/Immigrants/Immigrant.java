b9a3234d352449755156e9fc5c11d0a489e580b0
DimitarPaunov <dimitar.paunov1998@abv.bg>
2020-06-04 14:43:06 +0300

    Generating hashcode and equals for Immigrant by City name

--- src/Immigrants/Immigrant.java
+++ src/Immigrants/Immigrant.java
@@ -113,5 +113,30 @@ public abstract class Immigrant {
 	public String toString() {
 		return "Immigrant [passport=" + passport + ", money=" + money + ", city=" + city + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Immigrant other = (Immigrant) obj;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		return true;
+	}
 		
 }
